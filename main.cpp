fa06d69a961a2a9fc777246cd705cbba6cf6609e
Liam Speakman <ls173818@ohio.edu>
2020-08-28 09:20:38 -0400

    Added user input

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
 {
     cout << "Hello World" << endl;
+    
+    string name;
+    int age;
+
+    cout << "Input name" << endl;
+    cin >> name; 
+    cout << "Hello " <<  name  << '!'<< endl;
+    cout << "Input age" << endl;
+    cin >> age; 
+    cout << "You are " << age << " years old." << endl;
+
     return 0;
 }
