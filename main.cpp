ab62c934ffd1c666c0024ac3e6e8222782b124df
Liam Speakman <ls173818@ohio.edu>
2020-08-28 12:41:25 -0400

    Added more documentation

--- main.cpp
+++ main.cpp
@@ -6,22 +6,19 @@ using namespace std;
  * @author: Liam Speakman
  * @date: August 28, 2020 
  * @brief: asks for name and age and gives personalized greeting.
- * 
  */
-int main()
+int main() /// ENTRY POINT
 {
-    cout << "Hello World" << endl;
-    
+    // Variables for name and age    
     string name;
     int age;
-
+    // Input name and age and reply with personalized greetings respectively.
     cout << "Input name" << endl;
     cin >> name; 
     cout << "Hello " <<  name  << '!'<< endl;
     cout << "Input age" << endl;
     cin >> age; 
     cout << "You are " << age << " years old." << endl;
-    
-
+    /// END
     return 0;
 }
